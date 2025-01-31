c6fc9f2da314f3eb0b7c862cd9489cae3c87ede2
Luis Barufi <luisbarufi@gmail.com>
2025-01-30 21:44:47 -0300

    adds Bootstrap for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  FLASH_CLASSES = {
+    success: "alert-success",
+    error: "alert-danger",
+    alert: "alert-warning",
+    notice: "alert-info"
+  }.freeze
+
+  def bootstrap_class_for(flash_type)
+    FLASH_CLASSES[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages
+    flash.map do |msg_type, message|
+      content_tag(:div, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show", role: "alert") do
+        safe_join([
+          content_tag(:button, '', class: "btn-close", data: { bs_dismiss: 'alert' }),
+          message
+        ])
+      end
+    end.join.html_safe
+  end
 end
