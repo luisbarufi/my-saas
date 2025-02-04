a6f9fcd8a160565714eaaec801f8bc06ea29494e
Luis Barufi <luisbarufi@gmail.com>
2025-02-04 20:54:13 -0300

    the user can be invited again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show resend_invitation ]
+
   def index
     @users = User.all
   end
 
+  def resend_invitation
+    if @user.invitation_accepted_at.present?
+      redirect_to users_path, alert: "User with email #{@user.email} has already accepted the invitation"
+    else
+      @user.invite!
+      redirect_to users_path, notice: "Invitation resent to #{@user.email}"
+    end
+  end
+
   def show
+  end
+  
+  private
+  
+  def set_user
     @user = User.find(params[:id])
   end
 end
