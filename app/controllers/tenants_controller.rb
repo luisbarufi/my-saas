3f6e3d4ef146fd4a1b61e4d86064e3449e8d1a35
Luis Barufi <luisbarufi@gmail.com>
2025-02-10 21:38:31 -0300

    Add button for user to switch between tenants

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,9 +1,18 @@
 class TenantsController < ApplicationController
-  before_action :set_tenant, only: %i[ show edit update destroy ]
+  before_action :set_tenant, only: %i[ show edit update destroy switch ]
 
   # GET /tenants or /tenants.json
   def index
     @tenants = Tenant.all
+  end
+
+  def switch
+    if current_user.tenants.include?(@tenant)
+      current_user.update_attribute(:tenant_id, @tenant.id)
+      redirect_to my_tenants_path, notice: "Switched to tenant: #{current_user.tenant.name}"
+    else
+      redirect_to my_tenants_path, alert: "You are not authorized to access tenant: #{@tenant.name}"
+    end
   end
 
   def my
