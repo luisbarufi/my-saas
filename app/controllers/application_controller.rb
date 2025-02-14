9775e8c296ebe3bcac2716c932681cdee93fcf82
Luis Barufi <luisbarufi@gmail.com>
2025-02-14 19:53:18 -0300

    move logic for setting a tenant to a set_tenant concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
-  set_current_tenant_through_filter
-  before_action :set_tenant
-
-  def set_tenant
-    return set_current_tenant(nil) unless current_user
-  
-    tenant = current_user.tenant
-    if tenant.present? && current_user.tenants.include?(tenant)
-      set_current_tenant(tenant)
-    else
-      set_current_tenant(nil)
-    end
-  end
+  include SetTenant
 end
---
+++ app/controllers/concerns/set_tenant.rb
@@ -0,0 +1,19 @@
+module SetTenant
+  extend ActiveSupport::Concern
+
+  included do
+    set_current_tenant_through_filter
+    before_action :set_tenant
+  
+    def set_tenant
+      return set_current_tenant(nil) unless current_user
+    
+      tenant = current_user.tenant
+      if tenant.present? && current_user.tenants.include?(tenant)
+        set_current_tenant(tenant)
+      else
+        set_current_tenant(nil)
+      end
+    end
+  end
+end
