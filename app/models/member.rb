77c619a099107f0f766a4f1c104a335957497e62
Luis Barufi <luisbarufi@gmail.com>
2025-02-25 21:22:25 -0300

    global and tenant-specific views

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,10 +1,8 @@
 class Member < ApplicationRecord
   belongs_to :user
-  belongs_to :tenant
 
   validates :tenant_id, presence: true
   validates_uniqueness_of :user_id, scope: :tenant_id
 
   acts_as_tenant(:tenant)
-  # validates_uniqueness_to_tenant :user_id
 end
