38c3a662c31338d32eb75fbdf6b313e690ee3722
Luis Barufi <luisbarufi@gmail.com>
2025-04-13 23:04:44 -0300

    there has to be at least one admin member in a tenant

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,7 @@ class Member < ApplicationRecord
   validates :tenant_id, presence: true
   validates_uniqueness_of :user_id, scope: :tenant_id
   validate :must_have_a_role, on: :update
+  validate :must_have_an_admin
 
   acts_as_tenant(:tenant)
 
@@ -26,6 +27,12 @@ class Member < ApplicationRecord
   def must_have_a_role
     if self.roles.values.none?
       errors.add(:base, "A member must have at least one role")
+    end
+  end
+
+  def must_have_an_admin
+    if (self.tenant.members.pluck(:roles).count { |h| h[:'admim'] == true } == 1) && (roles_changed? && admim == false)
+      errors.add(:base, "The tenant has to have at least one admim")
     end
   end
 end
