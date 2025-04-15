bc3e399333b0971fa97eb6110b5c2c88fb21b3a7
Luis Barufi <luisbarufi@gmail.com>
2025-04-14 22:23:43 -0300

    basic validation for Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :tenant
+
+  validates :first_name, :last_name, :phone_number, :email, presence: true
+  validates_uniqueness_of :phone_number, scope: :tenant_id
 end
