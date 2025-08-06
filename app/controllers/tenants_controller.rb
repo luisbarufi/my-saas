b35c38fa34e0a6b8940a04243e186792bce95ac9
Luis Barufi <luisbarufi@gmail.com>
2025-08-05 22:45:54 -0300

    install ActiveStorage

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -86,7 +86,7 @@ class TenantsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tenant_params
-      params.require(:tenant).permit(:name, :plan)
+      params.require(:tenant).permit(:name, :plan, :logo)
     end
 
     def require_admin
